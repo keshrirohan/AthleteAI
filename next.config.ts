3ef421fb3739e59a8162f6b131a5f43f02f3e287
Chetan thakur <chetanthakur0507@gmail.com>
2025-09-11 18:24:08 +0530

    pwa work is complete

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+import withPWA from 'next-pwa';
 import type { NextConfig } from "next";
 
+declare module 'next-pwa';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+    manifest: "/manifest.json"
+  },
+  // ...other config options
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
